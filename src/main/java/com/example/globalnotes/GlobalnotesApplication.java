954ebdaa2b251822052b4a9e1945a34b92b3f3fd
Alexander <alexander_reztsov@mail.ru>
2021-06-01 06:20:15 +0300

    Initial commit

---
+++ src/main/java/com/example/globalnotes/GlobalnotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.globalnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalnotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalnotesApplication.class, args);
+    }
+
+}
